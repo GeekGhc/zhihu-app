75ac07adaa6e1408e8b2f77e451afcddba08e551
GeekGhc <ghcgavin@sina.com>
2017-04-03 20:18:00 +0800

    密码修改

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChangePasswordRequest;
 use Illuminate\Http\Request;
+use Hash;
 
 class PasswordController extends Controller
 {
@@ -14,8 +16,17 @@ class PasswordController extends Controller
     }
 
     //密码修改
-    public function update()
+    public function update(ChangePasswordRequest $request)
     {
+        if(Hash::check($request->get('old_password'),user()->password)){
+            user()->password = bcrypt($request->get('password'));
+            user()->save();
+            flash('密码修改成功','success');
 
+            return back();
+        }
+
+        flash('密码修改失败','danger');
+        return back();
     }
 }
--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -13,7 +13,7 @@ class ChangePasswordRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,19 @@ class ChangePasswordRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'old_password'=>'required|min:6',
+            'password' => 'required|min:6|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'old_password.required'=>'原始密码不能为空',
+            'old_password.min'=>'原始密码不能少于6个字符',
+            'password.required'=>'新密码不能为空',
+            'password.min'=>'原始密码不能少于6个字符',
+            'password.confirmed'=>'两次输入的新密码不符',
         ];
     }
 }
